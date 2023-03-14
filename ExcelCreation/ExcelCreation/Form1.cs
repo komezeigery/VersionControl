e023bec11cb8b72a785e1eae28ea5b50960edf22
Gergely Máté Kőmezei <gergely.komezei@stud.uni-corvinus.hu>
2023-03-14 09:29:16 +0100

    ExcelCreation

--- ExcelCreation/ExcelCreation/Form1.cs
+++ ExcelCreation/ExcelCreation/Form1.cs
@@ -79,7 +79,52 @@ namespace ExcelCreation
             {
                 xlSheet.Cells[1, i + 1] = headers[i];
             }
+
+            object[,]values = new object[flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (Flat flat in flats)
+            {
+                values[counter, 0] = flat.Code;
+                values[counter, 1] = flat.Vendor;
+                values[counter, 2] = flat.Side;
+                values[counter, 3] = flat.District;
+                values[counter, 4] = flat.Elevator;
+                if (flat.Elevator)
+                    values[counter, 4] = "Igen";
+                else
+                    values[counter, 4] = "nem";
+                values[counter, 5] = flat.NumberOfRooms;
+                values[counter, 6] = flat.FloorArea;
+                values[counter, 7] = flat.Price;
+                values[counter, 8] = flat.Price / flat.FloorArea;
+
+                counter++;
+            }
+            xlSheet.get_Range(GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            
+
         }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
 
         private void LoadData()
         {
