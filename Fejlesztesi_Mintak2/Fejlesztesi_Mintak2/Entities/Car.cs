8b5bd5f7300a455e63912fb43155f6b18a7e3dcc
Gergely Máté Kőmezei <gergely.komezei@stud.uni-corvinus.hu>
2023-04-11 09:07:13 +0200

    commit

--- Fejlesztesi_Mintak2/Fejlesztesi_Mintak2/Entities/Car.cs
+++ Fejlesztesi_Mintak2/Fejlesztesi_Mintak2/Entities/Car.cs
@@ -5,18 +5,14 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Fejlesztesi_Mintak2.Entities
 {
-    public class Ball : Toy
+    public class Car : Toy
     {
-
         protected override void DrawImage(Graphics g)
         {
-            var brush = new SolidBrush(Color.Blue);
-            g.FillEllipse(brush, 0, 0, Width, Height);
+           
         }
-    
     }
 }
